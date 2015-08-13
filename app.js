147aacb36907c6ca4763da35f44a5aaff0892134
Matthew Barbe <matthew.barben@anvilation.com>
2015-08-12 23:32:09 -0700

    Formatting

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ $(document).ready(function() {
 
 	var supportFolder = supporting.hasAttachment(uploadFolder, token),
 		addVersion = 0,
-        supportFiles;
+		supportFiles;
 
 	supportFolder.done(function(result) {
 		var resultLength = result.length;
-        console.log(result);
+		console.log(result);
 		if (!resultLength) {
 			$('#supportingDocuments').html('<p class="lead">No Supporting Information Uploaded</p>');
 
@@ -41,39 +41,39 @@ $(document).ready(function() {
 	}).fail(function(error) {
 		alert(error);
 	});
-    
-    
-    //$("#supportingDocument").on('change', prepareUpload);
-    
+
+
+	//$("#supportingDocument").on('change', prepareUpload);
+
 	$("#supportingDocument").change(function() {
-		
-        var filename = $(this).val();
-        filename = filename.split('/').pop().split('\\').pop();
-        console.log(filename);
-        var fd = new FormData();
-        fd.append("type", "144");
-        fd.append("parent_id", uploadFolder);
-        fd.append("name", filename);
-        fd.append("file", document.getElementById( 'supportingDocument' ).files[ 0 ]);
-        var url = '/otcs/cs.exe/api/v1/nodes/';
+
+		var filename = $(this).val();
+		filename = filename.split('/').pop().split('\\').pop();
+		console.log(filename);
+		var fd = new FormData();
+		fd.append("type", "144");
+		fd.append("parent_id", uploadFolder);
+		fd.append("name", filename);
+		fd.append("file", document.getElementById('supportingDocument').files[0]);
+		var url = '/otcs/cs.exe/api/v1/nodes/';
 
 
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: fd,
-            contentType: false,
-            processData: false,
-            beforeSend: function( xhr ) {
-                // Add the ticket from the Autheticate function to the request header.
-                xhr.setRequestHeader( "otcsticket", token ),
-                xhr.overrideMimeType( "multipart/form-data" )
-            }
-        }).done(function( data ) {
-            console.log( data );
-        }).fail(function( request, statusText ) {
-            console.log( statusText );
-        });
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: fd,
+			contentType: false,
+			processData: false,
+			beforeSend: function(xhr) {
+				// Add the ticket from the Autheticate function to the request header.
+				xhr.setRequestHeader("otcsticket", token),
+					xhr.overrideMimeType("multipart/form-data")
+			}
+		}).done(function(data) {
+			console.log(data);
+		}).fail(function(request, statusText) {
+			console.log(statusText);
+		});
 
 	});
 
@@ -81,33 +81,33 @@ $(document).ready(function() {
 
 });
 
-    $("#initDocumentAddVersion").change(function() {
-		var fd = new FormData();
-        fd.append("id", uploadDocument );
-        fd.append("file", document.getElementById( 'initDocumentAddVersion' ).files[ 0 ]);
-        var url = '/otcs/cs.exe/api/v1/nodes/' + uploadDocument + '/versions';
+$("#initDocumentAddVersion").change(function() {
+	var fd = new FormData();
+	fd.append("id", uploadDocument);
+	fd.append("file", document.getElementById('initDocumentAddVersion').files[0]);
+	var url = '/otcs/cs.exe/api/v1/nodes/' + uploadDocument + '/versions';
 
 
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: fd,
-            contentType: false,
-            processData: false,
-            beforeSend: function( xhr ) {
-                // Add the ticket from the Autheticate function to the request header.
-                xhr.setRequestHeader( "otcsticket", token ),
-                xhr.overrideMimeType( "multipart/form-data" )
-            }
-        }).done(function( data ) {
-            console.log( data );
-        }).fail(function( request, statusText ) {
-            console.log( statusText );
-        });
-
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: fd,
+		contentType: false,
+		processData: false,
+		beforeSend: function(xhr) {
+			// Add the ticket from the Autheticate function to the request header.
+			xhr.setRequestHeader("otcsticket", token),
+				xhr.overrideMimeType("multipart/form-data")
+		}
+	}).done(function(data) {
+		console.log(data);
+	}).fail(function(request, statusText) {
+		console.log(statusText);
 	});
-function prepareUpload(event)
-{
-  supportFiles = event.target.files;
-    console.log(files);
+
+});
+
+function prepareUpload(event) {
+	supportFiles = event.target.files;
+	console.log(files);
 }
