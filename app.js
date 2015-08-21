0201138b04a3e9b9567bef2f233c37f7ed4d163e
Matthew Barbe <matthew.barben@anvilation.com>
2015-08-20 17:38:06 -0700

    Complete Initial Refactor

--- app.js
+++ app.js
@@ -1,99 +1,85 @@
 /* The purpose of this is show:
- - how a new document can be uploaded to an Attachment volume / folder
+ - how a new document can be uploaded to an Content Server conainter
  - how to add a version to a an existing document
 
 
 
 */
-// Sample Documents
+// Sample Data
 var sampleData = {
 		folder: 0, //Folder
 		document: 0, //Document
 		user: "otadmin@otds.admin", //username
 		password: "livelink", //password
-		url: "http://otcs105.exchange.loc/otcs/cs.exe/api/v1", //url to rest api
-		token: null,
-
-
-	},
-    authenticate = function(url, user, password) { // authentication method
-        var promise = $.Deferred();
-        url = url + '/auth';
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: {
-                username: user,
-                password: password
-            },
-            success: function(data) {
-                promise.resolve(data.ticket);
-            },
-            error: function() {
-                var error = "Invalid username/password specified";
-                promise.reject(error);
-            }
-        });
-        return promise;
-    },
-    displayDocuments = function(folder, token) {
-        var promise = $.Deferred(),
-            url = sampleData.url + '/nodes/' + folder + '/nodes';
-        $.ajax({
-            type: "GET",
-            url: url,
-            headers: {
-                "otcsticket": token
-            },
-            success: function(data) {
-                promise.resolve(data.data);
-
-
-
-            },
-            error: function() {
-                var error = 'an error occured returning Folder information';
-                promise.reject(error);
-            }
-        });
-        return promise;
-
-    };
+		url: "http://host/otcs/cs.exe/api/v1"
+};
+var authToken = Cookies.get('anl_authToken');
 
 $(document).ready(function() {
-	authenticate(sampleData.url, sampleData.user, sampleData.password).done(function(result) {
-		var message = $('<p></p>'),
-			output = $('#output');
-		message.append('Auth Token: ' + result);
-		output.html(message);
-		sampleData.token = result;
-		$('.document').show();
-
-		displayDocuments(sampleData.folder, sampleData.token).done(function(result) {
-			var resultLength = result.length,
-				output = $('#output'),
-				message = $('<p></p>'),
-				documents = $('<ul></ul>');
-			console.log(result);
-			message.append(JSON.stringify(result));
-			output.append(message);
-			if (!resultLength) {
-				$('#supportingDocuments').html('<p class="lead">No Supporting Information Uploaded</p>');
-			} else {
-				var document = "";
-				for (var x = 0; x > resultLength; x++) {
-					document = document + '<li>' + result.name + '<li/>';
-				}
-				documents.append(document);
-				$('#supportingDocuments').append(documents);
-			}
-		});
-	}).fail(function(error) {
-		$('#output').text(error);
-	});
-
+   //Cookies.remove('anl_authToken');
+    authToken ? displayDocuments (sampleData.url, sampleData.folder, authToken) : authenticate(sampleData.url, sampleData.user, sampleData.password, sampleData.folder);
 
 });
+
+
+//Authenticate and set auth token cookie
+function authenticate (url, user, pass, folder){
+    
+    var authurl = url + '/auth';
+    $.ajax({
+        type: "POST",
+        url: authurl,
+        data: {
+            username: user,
+            password: pass
+        },
+        success: function(data) {
+            Cookies.set('anl_authToken', data.ticket, { expires: 0 });
+            displayDocuments (url, folder, data.ticket)
+        },
+        error: function() {
+            var error = "Invalid username/password specified";
+             $('#output').text(error);
+        }
+    });
+}
+
+//  return documents 
+function displayDocuments (url, folder, token){
+    url = url + '/nodes/' + folder + '/nodes';
+    $.ajax({
+        type: "GET",
+        url: url,
+        headers: {
+            "otcsticket": token
+        },
+        success: function(data) {
+            renderDocuments(data.data);
+        },
+        error: function() {
+            var error = 'an error occured returning Folder information';
+            $('#output').text(error);
+        }
+    });
+    
+}
+//render documents
+function renderDocuments (result){
+    var resultLength = result.length,
+        folderData = $('#folderData'),
+        message = $('<p><Target folder Data Returned/p>');
+    folderData.find('tbody').empty();
+    if (resultLength === 0) {
+        $('#supportingDocuments').html('<p class="lead">No Supporting Information Uploaded</p>');
+    } else {
+        var document = "";
+        for (i = 0; i < resultLength; i++) {
+            document = document + '<tr><td><img src=' + result[i].icon + ' border=0 alt=' + result[i].mime_type + ' /></td>' + '<td>' + result[i].name + '</td>' + '<td>' + result[i].size_formatted + '</td></tr>';
+        }
+        folderData.find('tbody').append(document);
+    }
+}
+
 
 // Add Version to Document 
 $("#initDocumentAddVersion").change(function() {
@@ -110,13 +96,14 @@ $("#initDocumentAddVersion").change(function() {
 		processData: false,
 		beforeSend: function(xhr) {
 			// Add the ticket from the Autheticate function to the request header.
-			xhr.setRequestHeader("otcsticket", sampleData.token),
+			xhr.setRequestHeader("otcsticket", authToken),
 				xhr.overrideMimeType("multipart/form-data")
 		}
 	}).done(function(data) {
-		console.log(data);
+		displayDocuments (sampleData.url, sampleData.folder, authToken);
+        
 	}).fail(function(request, statusText) {
-		console.log(statusText);
+        $('#output').text(statusText);
 	});
 
 });
@@ -125,10 +112,11 @@ $("#supportingDocument").change(function() {
 
 	var filename = $(this).val(),
         url = sampleData.url + '/nodes/',
+        formEl = $(this),
         fd = new FormData();// Create FormData Object
             
 	filename = filename.split('/').pop().split('\\').pop();
-	console.log(filename);
+	
 	fd.append("type", "144");
 	fd.append("parent_id", sampleData.folder);
 	fd.append("name", filename);
@@ -144,13 +132,15 @@ $("#supportingDocument").change(function() {
 		processData: false,
 		beforeSend: function(xhr) {
 			// Add the ticket from the Autheticate function to the request header.
-			xhr.setRequestHeader("otcsticket", sampleData.token),
+			xhr.setRequestHeader("otcsticket", authToken),
 				xhr.overrideMimeType("multipart/form-data")
 		}
 	}).done(function(data) {
-		console.log(data);
-	}).fail(function(request, statusText) {
-		console.log(statusText);
+        $('#output').text(JSON.stringify(data));
+        displayDocuments (sampleData.url, sampleData.folder, authToken);
+        formEl.replaceWith( formEl = formEl.clone( true ) );
+    }).fail(function(request, statusText) {
+		$('#output').text(statusText);
 	});
 
 });
